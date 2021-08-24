01ddb4d0890a96022807a4fc0ca7d33a7192ea9b
shubhamsinha <shubhamsinha.msg2@gmail.com>
2021-08-24 18:18:06 +0200

    debug prints

--- Camera/camerarec.py
+++ Camera/camerarec.py
@@ -10,8 +10,6 @@ import connection_and_network_constants
 
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# address '0.0.0.0' or '' work to allow connections from other machines.  'localhost' disallows external connections.
-# see https://www.raspberrypi.org/forums/viewtopic.php?t=62108
 serv.bind(('0.0.0.0', connection_and_network_constants.SOCKET_PORT))
 serv.listen(5)
 print("Ready to accept 5 connections")
@@ -31,20 +29,20 @@ while True:
     shape_string = ''
     while True:
         # print('waiting for data')
-        # Try 4096 if unsure what buffer size to use. Large transfer chunk sizes (which require large buffers) can cause corrupted results
         data = conn.recv(connection_and_network_constants.BUFFER_SIZE)
         if not data or data == b'tx_complete':
             break
         elif shape is None:
             shape_string += data.decode("utf-8")
             # Find the end of the line.  An index other than -1 will be returned if the end has been found because
-            # it has been received
+            print('shape:::::::::::::', shape)
             if shape_string.find('\r\n') != -1:
                 width_index = shape_string.find('width:')
                 height_index = shape_string.find('height:')
                 width = int(shape_string[width_index + len('width:'): height_index])
                 height = int(shape_string[height_index + len('height:'): ])
                 shape = (width, height)
+                print(shape)
             print("shape is {}".format(shape))
         else:
             chunks_received += 1
@@ -53,7 +51,9 @@ while True:
             # print(array_from_client)
         conn.sendall(b'ack')
         # print("sent acknowledgement")
-    #     TODO: need to check if sending acknowledgement of the number of chunks and the total length of the array is a good idea
+    #     
+    #doesnto required, the image we can recive withot this,
+    #anxhela -> check here
     print("chunks_received {}. Number of bytes {}".format(chunks_received, len(array_from_client)))
     img: Image.Image = create_image_from_bytes(array_from_client)
     img.show()
